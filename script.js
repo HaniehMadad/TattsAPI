a82dc750b7aa9cf42ff88ec58121a0e1f3d86897
HaniehMadad <hanieh.md@gmail.com>
2016-08-26 17:11:56 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+﻿"use strict";
+
+(function() {
+
+    var app = angular.module("app", []);
+
+    var greyhoundMeetingsController = function($http) {
+        var vm = this;
+        var today = new Date();
+        vm.title = "Date: " + today.toDateString();
+        vm.meetings = [];
+        vm.meetingName = "G"; //Greyhound
+        vm.vanueList = [
+            //SA venueu
+            "Ipswich",
+            "Angle Park",
+            "Gawler",
+            "Mount Gambier",
+            "Port Augusta",
+            "Strathalbyn"
+        ];
+        var url = "https://api.ubet.com/sales/vmax/web/data/racing/" + today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate();
+
+        $http.get(url).success(function(data) {
+            var meetings = data.RaceDay.Meetings;
+            vm.meetings = meetings;
+        });
+
+        var onError = function(reason) {
+            vm.error = "Could not fetch the data";
+        };
+    };
+
+    app.controller("GreyhoundMeetings", ["$http", greyhoundMeetingsController]);
+}());
